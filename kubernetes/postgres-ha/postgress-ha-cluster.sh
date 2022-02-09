b91080593d8094011ecf995111b0ff0716fd3aa1
devil1281 <darvin.w@freenet.de>
2022-02-09 17:37:52 +0100

    update

--- kubernetes/postgres-ha/postgress-ha-cluster.sh
+++ kubernetes/postgres-ha/postgress-ha-cluster.sh
@@ -1,2 +1,2 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install postgres-ha bitnami/postgresql-ha -n postgres
+helm install postgres-ha bitnami/postgresql-ha --values values.yaml
